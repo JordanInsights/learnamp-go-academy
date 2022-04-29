99acd7eb0961d623d74915a5d86e9e44258bb8c1
JordanInsights <jordan.shaw2400@icloud.com>
2022-04-29 16:39:16 +0100

    Change initial array to slice and make copies of slices rather than modifying original with each transformation

--- assignment-4/Main.go
+++ assignment-4/Main.go
@@ -14,25 +14,31 @@ func main() {
 	min := 1
 	max := 11
 
-	var collection [10]int
-
-	i := 0
-	for i < 10 {
-		collection[i] = rand.Intn(max-min) + min
-		i += 1
+	unsortedSlice := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		unsortedSlice[i] = rand.Intn(max-min) + min
 	}
 
 	fmt.Print("\n")
 
-	fmt.Print("Unsorted Array: ", collection, "\n")
+	// (i) Unsorted
+	fmt.Print("Unsorted Slice: ", unsortedSlice, "\n")
 
-	sort.Ints(collection[:])
-	fmt.Print("Ascending sequential order: ", collection, "\n")
+	// (ii) Sorted in ascending order
+	ascendingSlice := make([]int, 10)
+	copy(ascendingSlice, unsortedSlice)
+	sort.Ints(ascendingSlice)
+	fmt.Print("Ascending sequential order: ", ascendingSlice, "\n")
 
-	sort.Slice(collection[:], func(i, j int) bool {
-		return collection[i] >= collection[j]
+	// (iii) Sorted in descending order
+	descendingSlice := make([]int, 10)
+	copy(descendingSlice, unsortedSlice)
+	sort.Slice(descendingSlice, func(i, j int) bool {
+		return descendingSlice[i] >= descendingSlice[j]
 	})
-	fmt.Print("Descending sequential order: ", collection, "\n")
+	fmt.Print("Descending sequential order: ", descendingSlice, "\n")
+
+	// (iv) Count of odd and even numbers from odd
 
 	fmt.Print("\n")
 }
